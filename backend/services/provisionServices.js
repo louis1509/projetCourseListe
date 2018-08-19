8d9b25f9e2360b5f0428084a84b0c8eede0efc5b
louis1509 <louisbeucherieapb@hotmail.com>
2018-08-19 23:36:02 +0200

    [BACK] began provision services

--- backend/services/provisionServices.js
+++ backend/services/provisionServices.js
@@ -6,6 +6,11 @@ const Provision = require('../models/Provisions');
 const ProvisionServices = {
 	createProvision(req, res, next){
 		console.log('Create provision');
+		let newProvision = {};
+		newProvision.price = req.price;
+		newProvision.quantity = req.quantity;
+		newProvision.buy = false;
+		}
 
 	}
 
